84c77ffb857a20a08ead6e7ff535bc4f78744a74
SeanGau <rrtw0627@gmail.com>
2020-10-21 21:43:02 +0800

    add joystick

---
+++ ARCard/Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Joystick joystick;
+
+
+    public Transform target { get; set; }
+    private void Update()
+    {
+        ControlTarget();
+    }
+
+    void ControlTarget()
+    {
+        if (target)
+        {
+            target.localScale += joystick.Vertical * Time.deltaTime * Vector3.one / 10;
+            target.Rotate(0, joystick.Horizontal * Time.deltaTime * 100, 0);
+        }
+    }
+}
